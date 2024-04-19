db9cab87991ec45b9132229d27bcf665c8e32e03
alstdu <lexi.dugo@gmail.com>
2024-04-19 11:36:05 -0700

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+/* global fullpage */
+new fullpage( '#fullpage', {
+    menu: '#myMenu',
+    navigation: true,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    autoScrolling: true,
+    scrollHorizontally: true,
+    scrollBar: false,
+    keyboardScrolling: true,
+    fixedElements: '#header',
+    sectionsColor: ['#fafafa', '#fafafa', '#fafafa', '#fafafa'],
+    licenseKey: 'gplv3-license',
+    verticalCentered: true,
+    navigationTooltips: ['home','projects', 'skills', 'about me'],
+    scrollOverflow: true,
+        onScrollOverflow: function(section, slide, position, direction){
+            // eslint-disable-next-line no-unused-vars
+            var params = {
+                section: section,
+                slide: slide,
+                position: position,
+                direction: direction,
+            };
+            console.log(params);
+},
+});
+
+console.log('helloworld');
