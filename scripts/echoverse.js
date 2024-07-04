3728f881f70bf2745dedaf66496ebe34e1edad44
alstdu <lexi.dugo@gmail.com>
2024-07-04 10:17:51 -0700

    Add echoverse js file

---
+++ scripts/echoverse.js
@@ -0,0 +1,31 @@
+const pages = ['intro', 'process', 'the figma'];
+
+// TODO: DRY - don't repeat yourself
+/* global fullpage */
+new fullpage( '#fullpage', {
+    menu: '#myMenu',
+    navigation: true,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    autoScrolling: true,
+    scrollHorizontally: true,
+    scrollBar: false,
+    keyboardScrolling: true,
+    fixedElements: '#header',
+    sectionsColor: ['#fafafa', '#fafafa', '#fafafa'],
+    licenseKey: 'gplv3-license',
+    verticalCentered: true,
+    navigationTooltips: pages,
+    scrollOverflow: true,
+    // this regex matches spaces - replace replaces the spaces with nothing (removes them)
+    anchors: pages.map( s => s.replace(/\s/g, '') ),
+    onScrollOverflow: function(section, slide, position, direction) {
+        // eslint-disable-next-line no-unused-vars
+        var params = {
+            section: section,
+            slide: slide,
+            position: position,
+            direction: direction,
+        };
+    },
+});
